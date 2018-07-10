bb93a7903d44c9cf2b06a771593c956849da72c9
siarasmall <siarasmall19@gmail.com>
2018-07-10 13:03:02 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -12,7 +12,7 @@ puts "Step 1: Right arm out in front of you, palm facing down."
 puts "Left arm out in front of you, palm facing down"
 
 # Step 3: FIXME
-puts "
+puts "Turn your right palm up."
 
 # Step 4: FIXME
 puts "Step 4: Turn your left palm up."
